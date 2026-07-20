1ba3761395926b6e830ede95c49d4ae9a3d45463
abdulrahman-mpt <abdulrahman.jimoh@moniepoint.com>
2026-07-20 17:44:18 +0100

    Bump version to 0.5.1 and update SHA256 hashes

--- Formula/eshu.rb
+++ Formula/eshu.rb
@@ -3,29 +3,29 @@ class Eshu < Formula
   homepage "https://github.com/Moniepoint/eshu"
   license "Proprietary"
 
-  version "0.5.0"
+  version "0.5.1"
 
   on_macos do
     on_arm do
       url "https://proxy-function-241114592952.europe-west1.run.app/download/v#{version}/eshu-#{version}.darwin-arm64.tar.gz"
-      sha256 "2a32cf429cbb088003511645dd7f8561f81775725464411b87149b082c7ff4bb"
+      sha256 "4a7a03d65b6efa28e8f235a6af2333f23016de3421e031dcef9f733c2e3388b6"
     end
 
     on_intel do
       url "https://proxy-function-241114592952.europe-west1.run.app/download/v#{version}/eshu-#{version}.darwin-amd64.tar.gz"
-      sha256 "0816d05ca83e60c9c46697774d3d3301c98b7e581b895d72096d15966aafaa7c"
+      sha256 "2fcac77c8947fb2f7dc7b21d6b8bea039004188540c6c56a84c3765014b8e4f8"
     end
   end
 
   on_linux do
     on_arm do
       url "https://proxy-function-241114592952.europe-west1.run.app/download/v#{version}/eshu-#{version}.linux-arm64.tar.gz"
-      sha256 "f92d84bfaf76d27c8ec61c3d8058c638c18dca82845f2a78e0ea625e218cf366"
+      sha256 "898a1173765a36ec74be9d819d7f39032a68979889a81f12dcd3103bfa3edcd3"
     end
 
     on_intel do
       url "https://proxy-function-241114592952.europe-west1.run.app/download/v#{version}/eshu-#{version}.linux-amd64.tar.gz"
-      sha256 "22f5320adc6a183ac6fb99d73a7a25f4b40c3a95e0525ed9fcbe34817a46ce34"
+      sha256 "61c7dfa6d83d6e77ef3e61a752a780e41490f312196942cbb2b2b712f9393389"
     end
   end
 
